e14021403dc3e44ee0ec9a13ee07e9fcfca7f9b7
Daniel Falbel <dfalbel@gmail.com>
2017-06-12 04:06:58 +0000

    update the sh command

--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
+# update backend repo
+
 cd brbloggers-backend
 git pull
 Rscript main.R
 git add -A
 git commit -m "new content"
 git push
+
+# update the blog itself
+# change to the submodule directory
+cd ../brbloggers/content/
+
+# checkout desired branch
+git checkout master
+
+# update
+git pull
+
+# get back to your project root
+cd ..
+
+# now the submodules are in the state you want, so
+git commit -am "submodule updated"

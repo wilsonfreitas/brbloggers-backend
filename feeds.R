a00ce2aeb9ab198a9aa80e4cd3aaa1ea111b50c9
Daniel Falbel <dfalbel@gmail.com>
2017-06-12 16:27:18 +0000

    adicionar analise real ao feed

--- feeds.R
+++ feeds.R
@@ -8,6 +8,11 @@ feeds <- list(
   "paixao-por-dados" = list(
     nome = "Paixão por Dados",
     url = "https://sillasgonzaga.github.io/feed.xml"
+  ),
+  
+  "analise-real" = list(
+    nome = "Análise Real",
+    url = "https://analisereal.com/feed/"
   )
   
 )
